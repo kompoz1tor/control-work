9cd1abf8dbfe8425cc0a2e4d1cdbe016841ae641
Sergey Zhavoronkov <zhavoronkov-s@mail.ru>
2022-08-13 14:55:55 +0300

    Работающий код

--- Program.cs
+++ Program.cs
@@ -1,24 +1,29 @@
 ﻿Console.WriteLine("Программа, которая принимает на вход массив данных. И выводит массив из существующего с элементами, длина которых меньше трех");
-Console.WriteLine("****************************************************************************************************");
+Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------------");
 
 
 Console.WriteLine("Введите колличество элементов в массиве");
 int FirstIndexNumber = Convert.ToInt32(Console.ReadLine());
 int SecondIndexNumber = 0;
-
+/************************************************************************************/
 string[] firstArray = new string[FirstIndexNumber];
-FillFirstArray(firstArray,  SecondIndexNumber);
-
+FillFirstArray(firstArray,ref SecondIndexNumber);
+Console.WriteLine("Исходный массив данных:");
 printArray(firstArray);
+Console.WriteLine("-------------------------------------------------------------------");
 
+Console.WriteLine("Колличество элементов массива, которые удовлетворяют наше условие:");
+Console.WriteLine(SecondIndexNumber);
+Console.WriteLine("-------------------------------------------------------------------");
 
 string[] secondArray = new string[SecondIndexNumber];
 FillCurrentArray(firstArray, secondArray);
-
+Console.WriteLine("Искомый массив данных:");
 printArray(secondArray);
+Console.WriteLine("-------------------------------------------------------------------");
 
-
-static string[] FillFirstArray(string[] array, int SecondIndexNumber)
+/************************************************************************************/
+string[] FillFirstArray(string[] array, ref int maxIndexArray)
     {
         Console.WriteLine();
 
@@ -27,27 +32,29 @@ static string[] FillFirstArray(string[] array, int SecondIndexNumber)
             Console.Write($"Введи {i+1} элемент: ");
             array[i] = Console.ReadLine();
             Console.WriteLine();
-            if (array[i].Length <= 3) SecondIndexNumber++;
+            if (array[i].Length <= 3) 
+            {
+                maxIndexArray = maxIndexArray + 1;
+            }
         }
         Console.WriteLine();
-
         return array;
     }
-
-static string[] FillCurrentArray(string[] firstArray, string[] secondArray)
+/************************************************************************************/
+string[] FillCurrentArray(string[] firstArray, string[] secondArray)
     {
         int j = 0;
         for (int i = 0; i < firstArray.Length; i++)
         {
             if (firstArray[i].Length <= 3)
             {
-                secondArray[j] += firstArray[i];
+                secondArray[j] = firstArray[i];
                 j++;
             }
         }
         return secondArray;
     }
-
+/************************************************************************************/
 static void printArray(string[] array)
     {
         for (int i = 0; i < array.Length; i ++)
@@ -56,3 +63,4 @@ static void printArray(string[] array)
         }
         Console.WriteLine();
     }
+/************************************************************************************/
