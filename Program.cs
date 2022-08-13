23f5c9efd158805d8bde6690194abfe7a0d2d5e8
Sergey Zhavoronkov <zhavoronkov-s@mail.ru>
2022-08-13 15:02:41 +0300

    Работающий код, описание методов

--- Program.cs
+++ Program.cs
@@ -5,11 +5,11 @@ Console.WriteLine("-------------------------------------------------------------
 Console.WriteLine("Введите колличество элементов в массиве");
 int FirstIndexNumber = Convert.ToInt32(Console.ReadLine());
 int SecondIndexNumber = 0;
-/************************************************************************************/
+
 string[] firstArray = new string[FirstIndexNumber];
 FillFirstArray(firstArray,ref SecondIndexNumber);
 Console.WriteLine("Исходный массив данных:");
-printArray(firstArray);
+PrintArray(firstArray);
 Console.WriteLine("-------------------------------------------------------------------");
 
 Console.WriteLine("Колличество элементов массива, которые удовлетворяют наше условие:");
@@ -19,10 +19,11 @@ Console.WriteLine("-------------------------------------------------------------
 string[] secondArray = new string[SecondIndexNumber];
 FillCurrentArray(firstArray, secondArray);
 Console.WriteLine("Искомый массив данных:");
-printArray(secondArray);
+PrintArray(secondArray);
 Console.WriteLine("-------------------------------------------------------------------");
 
-/************************************************************************************/
+/* FillFirstArray - Метод, который заполняет исходный массив данных, ввод с клавиатуры и заодно проверяющий, удовлетворяет ли вводимый элемент массива нашему словию (array[i].Lenght <= 3)
+Если элемент массива удовлетворяет условию, то длина искомого массива увеличивается на 1*/
 string[] FillFirstArray(string[] array, ref int maxIndexArray)
     {
         Console.WriteLine();
@@ -40,7 +41,7 @@ string[] FillFirstArray(string[] array, ref int maxIndexArray)
         Console.WriteLine();
         return array;
     }
-/************************************************************************************/
+/* FillCurrentArray - Метод, перебирает элементы исходного массива, и если он (элемент) меньше или равен 3, то его значение присвается в элемент искомого массива*/
 string[] FillCurrentArray(string[] firstArray, string[] secondArray)
     {
         int j = 0;
@@ -54,8 +55,8 @@ string[] FillCurrentArray(string[] firstArray, string[] secondArray)
         }
         return secondArray;
     }
-/************************************************************************************/
-static void printArray(string[] array)
+/* PrintArray - Универсальный метод вывода в консоль одномерного массива*/
+static void PrintArray(string[] array)
     {
         for (int i = 0; i < array.Length; i ++)
         {
